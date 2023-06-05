54c72a5a3dae77e5ff31364a5c083bb5ec802aa7
satyabratajena-study <135617223+satyabratajena-study@users.noreply.github.com>
2023-06-05 18:21:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit
 streamlit.title('my parents healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('  🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avocardo toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+
