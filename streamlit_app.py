f503856c2aa6bcc63ee48f417b8645b98380aeff
satyabratajena-study <135617223+satyabratajena-study@users.noreply.github.com>
2023-06-05 18:31:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocardo','Strawberry'])
